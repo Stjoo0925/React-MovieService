c7609ba33986caa61ad6712825b5a76122f70a38
Stjoo0925 <stjoo0925@gmail.com>
2024-10-16 00:12:31 +0900

    lecture: 7.3

--- src/App.js
+++ src/App.js
@@ -3,59 +3,41 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [amount, setAmount] = useState(0);
-  const [selectedCoinPrice, setSelectedCoinPrice] = useState(0);
-
-  const onChange = (event) => setAmount(event.target.value);
-  const onSelect = (event) => {
-    const selectedCoin = coins.find(
-      (coin) => coin.symbol === event.target.value
-    );
-    setSelectedCoinPrice(selectedCoin.quotes.USD.price);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
 
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-
+  console.log(movies);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      <span>코인선택 : </span>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onSelect}>
-          {coins.map((coin) => (
-            <option key={coin.id} value={coin.symbol}>
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
-      <br />
-      <span>구매할 금액(USD) : </span>
-      <input
-        onChange={onChange}
-        value={amount}
-        type="number"
-        placeholder="USD를 입력하세요..."
-      />
-      <br />
-      <span>구매가능 개수 : </span>
-      <span>
-        {loading
-          ? ""
-          : `${
-              selectedCoinPrice ? amount / selectedCoinPrice : "Select a coin"
-            }개`}
-      </span>
     </div>
   );
 }
